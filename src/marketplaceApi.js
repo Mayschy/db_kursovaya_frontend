8795ec3248bcf7b45c9f29cabe81d67811326908
Миша Език <mishunder9354@gmail.com>
2024-04-17 12:10:30 +0300

    refactoring

--- src/marketplaceApi.js
+++ src/marketplaceApi.js
@@ -24,235 +24,260 @@ export class PublicMarketplaceApi {
     }
 
     static sendEmailCode(email) {
-        return this.marketplaceClient.post(`/account/send_email_code`, qs.stringify({
-            "email": email
-        }))
+        return this.marketplaceClient.post(`/account/send_email_code`, {
+            params: {"email": email}
+        })
     }
 
     static findUsers(query, page, pageSize) {
-        return this.marketplaceClient.get(`/account/users`, qs.stringify({
-            "query": query,
-            "page": page,
-            "pageSize": pageSize
-        }))
+        return this.marketplaceClient.get(`/account/users`, {
+            params: {
+                "query": query,
+                "page": page,
+                "pageSize": pageSize
+            }
+        })
     }
 }
 
-export class PriveteMarketplaceApi {
-    static authHeaders
-    static authenitcatedClient
-
-    static initCreditans(username, password) {
+export class PrivateMarketplaceApi {
+    constructor(username, password) {
         this.authHeaders = new Headers()
         this.authHeaders.set('Authorization', 'Basic ' + btoa(username + ":" + password))
-        this.authenitcatedClient = axios.create({
+        this.authenticatedClient = axios.create({
             baseURL: MARKET_PLACE_ENDPOINT,
             headers: this.authHeaders
         })
     }
 
-    static uploadFile(multipart) {
-        return this.authenitcatedClient.post(`/file-storage/file`, {multipart}) // idk
+    uploadFile(multipart) {
+        return this.authenticatedClient.post(`/file-storage/file`, {multipart}) // idk
     }
 
-    static uploadFileByURL(filename, url) {
-        return this.authenitcatedClient.post(`/file-storage/file_by_url`, qs.stringify({
+    uploadFileByURL(filename, url) {
+        return this.authenticatedClient.post(`/file-storage/file_by_url`, qs.stringify({
             "name": filename,
             "url": url
         }))
     }
 
-    static fileInto(fileId) {
-        return this.authenitcatedClient.get(`/file-storage/${fileId}/info`)
+    fileInto(fileId) {
+        return this.authenticatedClient.get(`/file-storage/${fileId}/info`)
     }
 
-    static fileBytes(fileId) {
-        return this.authenitcatedClient.get(`/file-storage/${fileId}/bytes`)
+    fileBytes(fileId) {
+        return this.authenticatedClient.get(`/file-storage/${fileId}/bytes`)
     }
 
-    static getFile(fileId) {
-        return this.authenitcatedClient.get(`/file-storage/${fileId}`)
+    getFile(fileId) {
+        return this.authenticatedClient.get(`/file-storage/${fileId}`)
     }
 
-    static deleteFile(fileId) {
-        return this.authenitcatedClient.delete(`/file-storage/${fileId}`)
+    deleteFile(fileId) {
+        return this.authenticatedClient.delete(`/file-storage/${fileId}`)
     }
 
-    static getMe() {
-        return this.authenitcatedClient.delete(`/account/me`)
+    getMe() {
+        return this.authenticatedClient.delete(`/account/me`)
     }
 
-    static addShippingAddress(shippingAddress) {
-        return this.authenitcatedClient.post(`${MARKET_PLACE_ENDPOINT}/account/shipping_address`, qs.stringify({
+    addShippingAddress(shippingAddress) {
+        return this.authenticatedClient.post(`${MARKET_PLACE_ENDPOINT}/account/shipping_address`, qs.stringify({
             "shippingAddress": shippingAddress
         }))
     }
 
-    static removeShippingAddress(shippingAddress) {
-        return this.authenitcatedClient.delete(`${MARKET_PLACE_ENDPOINT}/account/shipping_address`, qs.stringify({
-            "shippingAddress": shippingAddress
-        }))
+    removeShippingAddress(shippingAddress) {
+        return this.authenticatedClient.delete(`${MARKET_PLACE_ENDPOINT}/account/shipping_address`, {
+            params: {
+                "shippingAddress": shippingAddress
+            }
+        })
     }
-    /** 
+
+    /**
      * Categories
      */
 
-    static registerCategory(registerRequest) {
-        return this.authenitcatedClient.post(`/categories/register`, registerRequest)
+    registerCategory(registerRequest) {
+        return this.authenticatedClient.post(`/categories/register`, registerRequest)
     }
 
-    static findCategory(query, page, pageSize) {
-        return this.authenitcatedClient.get(`/categories/find`, qs.stringify({
-            "query": query,
-            "page": page,
-            "pageSize": pageSize
-        }))
+    findCategory(query, page, pageSize) {
+        return this.authenticatedClient.get(`/categories/find`, {
+            params: {
+                "query": query,
+                "page": page,
+                "pageSize": pageSize
+            }
+        })
     }
 
-    static listCategory(page, pageSize) {
-        return this.authenitcatedClient.get(`/categories`, qs.stringify({
-            "page": page,
-            "pageSize": pageSize
-        }))
+    listCategory(page, pageSize) {
+        return this.authenticatedClient.get(`/categories`, {
+            params: {
+                "page": page,
+                "pageSize": pageSize
+            }
+        })
     }
 
-    static countCategories() {
-        return this.authenitcatedClient.get(`/categories/count`)
+    countCategories() {
+        return this.authenticatedClient.get(`/categories/count`)
     }
 
     /*
     * Products
     */
-    static registerProduct(registerRequest) {
-        return this.authenitcatedClient.post(`/products/register`, registerRequest)
+    registerProduct(registerRequest) {
+        return this.authenticatedClient.post(`/products/register`, registerRequest)
     }
 
-    static deleteProduct(productId) {
-        return this.authenitcatedClient.delete(`/products/${productId}`)
+    deleteProduct(productId) {
+        return this.authenticatedClient.delete(`/products/${productId}`)
     }
 
-    static findProducts(query, page, pageSize) {
-        return this.authenitcatedClient.get(`/products/find`, qs.stringify({
-            "query": query,
-            "page": page,
-            "pageSize": pageSize
+    findProducts(query, page, pageSize) {
+        return this.authenticatedClient.get(`/products/find`, {
+            params: {
+                "query": query,
+                "page": page,
+                "pageSize": pageSize
+            }
+        })
+    }
+
+    countProducts() {
+        return this.authenticatedClient.get(`/products/count`)
+    }
+
+    addProductImage(productId, imageLink) {
+        return this.authenticatedClient.post(`/products/${productId}/image`, qs.stringify({
+            "image": imageLink
         }))
     }
 
-    static countProducts() {
-        return this.authenitcatedClient.get(`/products/count`)
-    }
-
-    static editProduct(product) {
-        return this.authenitcatedClient.post(`/products/edit`, product)
+    deleteProductImage(productId, imageLink) {
+        return this.authenticatedClient.delete(`/products/${productId}/image`, {
+            params: {
+                "image": imageLink
+            }
+        })
     }
 
     /*
     * Discounts
     */
-    static registerDiscount(registerRequest) {
-        return this.authenitcatedClient.post(`/discounts/register`, registerRequest)
+    registerDiscount(registerRequest) {
+        return this.authenticatedClient.post(`/discounts/register`, registerRequest)
     }
 
-    static deleteDiscount(discountId) {
-        return this.authenitcatedClient.delete(`/discounts/${discountId}`)
+    deleteDiscount(discountId) {
+        return this.authenticatedClient.delete(`/discounts/${discountId}`)
     }
 
-    static listDiscounts(page, pageSize) {
-        return this.authenitcatedClient.get(`/discounts`, qs.stringify({
-            "page": page,
-            "pageSize": pageSize
-        }))
+    listDiscounts(page, pageSize) {
+        return this.authenticatedClient.get(`/discounts`, {
+            params: {
+                "page": page,
+                "pageSize": pageSize
+            }
+        })
     }
+
     /*
     * Orders
     */
 
-    static registerOrder(registerRequest) {
-        return this.authenitcatedClient.post(`/orders/register`, registerRequest)
+    registerOrder(registerRequest) {
+        return this.authenticatedClient.post(`/orders/register`, registerRequest)
     }
 
-    static cancelOrder(orderId) {
-        return this.authenitcatedClient.post(`/orders/cancel`, qs.stringify({
+    cancelOrder(orderId) {
+        return this.authenticatedClient.post(`/orders/cancel`, qs.stringify({
             "orderId": orderId
         }))
     }
 
-    static changeOrderStatus(orderId, newStatus) {
-        return this.authenitcatedClient.post(`/orders/${orderId}/change_status`, qs.stringify({
+    changeOrderStatus(orderId, newStatus) {
+        return this.authenticatedClient.post(`/orders/${orderId}/change_status`, qs.stringify({
             "newStatus": newStatus
         }))
     }
 
-    static listMyOrders(page, pageSize) {
-        return this.authenitcatedClient.get(`/orders/list_my`, qs.stringify({
-            "page": page,
-            "pageSize": pageSize
-        }))
+    listMyOrders(page, pageSize) {
+        return this.authenticatedClient.get(`/orders/list_my`, {
+            params: {
+                "page": page,
+                "pageSize": pageSize
+            }
+        })
     }
 
-    static countMyOrders() {
-        return this.authenitcatedClient.get(`/orders/count_my`)
+    countMyOrders() {
+        return this.authenticatedClient.get(`/orders/count_my`)
     }
 
-    static calculateOrder(registerRequest) {
-        return this.authenitcatedClient.get(`/orders/calculate`, registerRequest)
+    calculateOrder(registerRequest) {
+        return this.authenticatedClient.get(`/orders/calculate`, registerRequest)
     }
 
     /*
     * Favlists
     */
-    static createFavoriteList(registerRequest) {
-        return this.authenitcatedClient.post(`/favlists/create`, registerRequest)
+    createFavoriteList(registerRequest) {
+        return this.authenticatedClient.post(`/favlists/create`, registerRequest)
     }
 
-    static deleteFavoriteList(listId) {
-        return this.authenitcatedClient.delete(`/favlists/${listId}`)
+    deleteFavoriteList(listId) {
+        return this.authenticatedClient.delete(`/favlists/${listId}`)
     }
 
-    static changeFavoriteListVisibility(listId, isPublic) {
-        return this.authenitcatedClient.post(`/favlists/${listId}/change_visibility`, qs.stringify({
+    changeFavoriteListVisibility(listId, isPublic) {
+        return this.authenticatedClient.post(`/favlists/${listId}/change_visibility`, qs.stringify({
             "isPublic": isPublic
         }))
     }
 
-    static addProductToFavoriteList(listId, productId) {
-        return this.authenitcatedClient.post(`/favlists/${listId}/product/${productId}`)
+    addProductToFavoriteList(listId, productId) {
+        return this.authenticatedClient.post(`/favlists/${listId}/product/${productId}`)
     }
 
-    static removeProductFromFavoriteList(listId, productId) {
-        return this.authenitcatedClient.delete(`/favlists/${listId}/product/${productId}`)
+    removeProductFromFavoriteList(listId, productId) {
+        return this.authenticatedClient.delete(`/favlists/${listId}/product/${productId}`)
     }
 
-    static getMyFavoriteLists() {
-        return this.authenitcatedClient.get(`/favlists/my`)
+    getMyFavoriteLists() {
+        return this.authenticatedClient.get(`/favlists/my`)
     }
 
-    static getPublicFavoriteList(listId) {
-        return this.authenitcatedClient.get(`/favlists/${listId}/get_public`)
+    getPublicFavoriteList(listId) {
+        return this.authenticatedClient.get(`/favlists/${listId}/get_public`)
     }
 
-    static getUserPublicFavoriteLists(accountId) {
-        return this.authenitcatedClient.get(`/favlists/from_account/${accountId}`)
+    getUserPublicFavoriteLists(accountId) {
+        return this.authenticatedClient.get(`/favlists/from_account/${accountId}`)
     }
 
     /*
     * Comments
     */
 
-    static registerComment(registerRequest) {
-        return this.authenitcatedClient.post(`/comments/register`, registerRequest)
+    registerComment(registerRequest) {
+        return this.authenticatedClient.post(`/comments/register`, registerRequest)
     }
 
-    static deleteComment(commentId) {
-        return this.authenitcatedClient.delete(`/comments/${commentId}`)
+    deleteComment(commentId) {
+        return this.authenticatedClient.delete(`/comments/${commentId}`)
     }
 
-    static listComments(productId, page, pageSize) {
-        return this.authenitcatedClient.get(`/comments/from_product/${productId}`, qs.stringify({
-            "page": page,
-            "pageSize": pageSize
-        }))
+    listComments(productId, page, pageSize) {
+        return this.authenticatedClient.get(`/comments/from_product/${productId}`, {
+            params: {
+                "page": page,
+                "pageSize": pageSize
+            }
+        })
     }
 }
 
